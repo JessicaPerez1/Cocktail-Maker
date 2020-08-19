9ce6fbee75cfe5f28656e869daa89b683789b276
Jessica Perez <1jessicaperez@gmail.com>
2020-08-19 15:04:03 -0400

    Started my server

---
+++ server.js
@@ -0,0 +1,23 @@
+//DEPENDENCIES
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const app = express();
+//PORT
+const PORT = process.env.PORT || 4500;
+//MIDDLEWARES
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+//ROUTES
+
+//CONNECT MODELS
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethods", { useNewUrlParser: true });
+// const db = mongojs(databaseUrl, collections);
+
+//LISTENER
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
